b8495e7dbb4576b01f2b1e800bd1a4fc617178a1
MilkyLatte <jpqo9508@gmail.com>
2019-04-22 00:20:49 +0100

    lower parameters

--- assignment2_12345.pl
+++ assignment2_12345.pl
@@ -56,7 +56,7 @@ heuristic(Path, Target, Result) :-
     (   Fuel=0
     ->  H is Distance
     ;   otherwise
-    ->  H is 500 * 1/Fuel + Distance
+    ->  H is 100 * 1/Fuel + Distance
     ),
     length([First|Others], L),
     G is L,
@@ -132,7 +132,7 @@ addChildren(Children, CurrentPath, Agenda, InitialScore, Result) :-
     New is Score - InitialScore,
     print("SCORE:"),
     writeln(New),
-    (New < 40 -> 
+    (New < 20 -> 
       (   Type=empty
       ->  append([(Node, Type)], Path, NewPath),
           NewFuel is Fuel -1,
